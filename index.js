d9ffa0c82edc393e52e4a1238e83ee4970dc3aa6
Sayed Mubarish <mubarish007@gmail.com>
2025-06-11 12:04:58 +0530

    Create computer choice
    create a function getComputerChoice
    which return computer choice

---
+++ index.js
@@ -0,0 +1,7 @@
+function getComputerChoice(){
+    const randomNumber =  Math.floor(Math.random()*3 + 1)
+    if(randomNumber==1)return "rock"
+    else if(randomNumber==2) return "paper"
+    else return "scissors"
+}
+
